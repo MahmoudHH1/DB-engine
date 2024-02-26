d726fcf1ef607fb43b542499467d1aa74e9bc17b
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-02-26 22:14:50 +0200

    MetaData Class for csv file interaction

---
+++ Data/MetaData.java
@@ -0,0 +1,23 @@
+package Data;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MetaData {
+    static String filePath = "\\Data_Entry\\metadata.csv";
+    public static void writeDataToMetaDatafile( ArrayList<TableColumn> allColumns)
+    {
+        try {
+            FileWriter myWriter = new FileWriter(filePath , true);
+            for(TableColumn column : allColumns){
+                myWriter.append(column.toString());
+            }
+            myWriter.close();
+            System.out.println("Successfully wrote to the Metadata file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
