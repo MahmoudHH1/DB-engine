b903a8bb6cc72700f9d288b0813d3bcd8eec1cc1
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-02-26 21:16:47 +0200

    added method storePage to store page in .class file-- to be modified later -- Will probably be used in insert tuple method

--- Data/Table.java
+++ Data/Table.java
@@ -1,14 +1,13 @@
 package Data;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Table implements Serializable {
-    ArrayList<String>pagesPathes ;
+    ArrayList<String> pagesPathes ;
+
 
     ArrayList<TableColumn>allColumns;
 
@@ -33,7 +32,16 @@ public class Table implements Serializable {
         }
     }
 
-
+    public void storePage(Page p) throws IOException {
+        // In Progress !!!!!!!!!!
+        String path = allColumns.get(0).tableName + pagesPathes.size() +".class"; // need to know number of page to put
+        pagesPathes.add(path);                              // and tableName should belong to Table class not column
+        FileOutputStream fileOut = new FileOutputStream(path);
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(p);
+        out.close();
+        fileOut.close();
+    }
 
     public static void main(String[] args) {
 
