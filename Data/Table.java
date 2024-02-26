322103ec54a259fe659b81d25d966f16042c3aa3
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-02-26 22:14:50 +0200

    MetaData Class for csv file interaction

--- Data/Table.java
+++ Data/Table.java
@@ -14,23 +14,7 @@ public class Table implements Serializable {
 
     public Table(ArrayList<TableColumn> allColumns) {
         this.allColumns = allColumns;
-        writeDataToMetaDatafile("D:\\Coding\\DataBase\\Database_projects\\Database_Engine Project\\Database-Engine\\Data_Entry\\metadata.csv" , allColumns);
-    }
-
-
-    public static void writeDataToMetaDatafile(String filePath , ArrayList<TableColumn>allColumns)
-    {
-        try {
-            FileWriter myWriter = new FileWriter(filePath , true);
-            for(TableColumn column : allColumns){
-                myWriter.append(column.toString());
-            }
-            myWriter.close();
-            System.out.println("Successfully wrote to the Metadata file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        MetaData.writeDataToMetaDatafile(allColumns);
     }
 
 
