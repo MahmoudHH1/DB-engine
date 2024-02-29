da933a872eaaa15eba556ff80fe1e97526c67785
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-02-29 05:42:18 +0200

    Splited the code into packages and i added alose getters and setters

--- Data/Page/Page.java
+++ Data/Page/Page.java
@@ -1,32 +1,68 @@
-package Data;
+package Data.Page;
 
 //import javafx.scene.control.Tab;
+
+import Data.Handler.FileCreator;
+import Data.Table.Table;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Vector;
 public class Page implements Serializable {
-    Table table ;
-    String pageName ; // unnecessary attribute
-    Vector<Record> allRecords;
-    String pagePath ;
+    private Table table ;
+    private String pageName ; // unnecessary attribute
+    private Vector<Record> allRecords;
+    private String pagePath ;
     public Page (Table table) throws IOException {
         allRecords = new Vector<>();
         this.table = table ;
-        this.pageName = table.tableName + table.pageNum ;
-        table.pageNum++; // next page num
-        this.pagePath = table.tableDir+'/'+pageName ;
+        this.pageName = table.getTableName() + table.getPageNum() ;
+        table.setPageNum(table.getPageNum()+1); // next page num
+        this.pagePath = table.getTableDir()+'/'+pageName ;
         pageCreator();
 
     }
 
-    public void addRecord(Record record){
-        allRecords.add(record); // in progress
+    public void pageCreator() throws IOException {
+        table.addNewPage(this.pagePath);// store pagePath in corresponding table
+        FileCreator.storeAsObject(this , this.pagePath);
     }
 
-    public void pageCreator() throws IOException {
-        table.pages.add(this.pagePath); // store pagePath in corresponding table
-        FileCreator.storeAsObject(this , this.pagePath);
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+    public String getPageName() {
+        return pageName;
+    }
+
+    public void setPageName(String pageName) {
+        this.pageName = pageName;
+    }
+
+    public Vector<Record> getAllRecords() {
+        return allRecords;
+    }
+
+    public void setAllRecords(Vector<Record> allRecords) {
+        this.allRecords = allRecords;
+    }
+
+    public String getPagePath() {
+        return pagePath;
+    }
+
+    public void setPagePath(String pagePath) {
+        this.pagePath = pagePath;
+    }
+
+
+    public void addRecord(Record record){
+        allRecords.add(record); // in progress
     }
 
     public int getPageSize(){
--- Data/Page/Record.java
+++ Data/Page/Record.java
@@ -1,4 +1,4 @@
-package Data;
+package Data.Page;
 
 import java.util.Vector;
 
