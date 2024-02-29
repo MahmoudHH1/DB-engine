d4ef925ad0d074b8136240ad2ae3fdd0b747e539
Mahmoud Hany <88785389+MahmoudH6@users.noreply.github.com>
2024-02-29 17:23:08 +0200

    adding to the insert method

--- DBApp.java
+++ DBApp.java
@@ -14,105 +14,121 @@ import java.util.Hashtable;
 public class DBApp {
 
 
+    public DBApp() {
 
-	public DBApp( ){
-		
-	}
+    }
 
-	// this does whatever initialization you would like
-	// or leave it empty if there is no code you want to 
-	// execute at application startup 
-	public void init( ){
-		
-		
-	}
+    // this does whatever initialization you would like
+    // or leave it empty if there is no code you want to
+    // execute at application startup
+    public void init() {
 
 
-	// following method creates one table only
-	// strClusteringKeyColumn is the name of the column that will be the primary
-	// key and the clustering column as well. The data type of that column will
-	// be passed in htblColNameType
-	// htblColNameValue will have the column name as key and the data 
-	// type as value
-	public void createTable(String strTableName,
-							String strClusteringKeyColumn,  
-							Hashtable<String,String> htblColNameType) throws DBAppException, IOException {
-		ArrayList<TableColumn> allColumns = new ArrayList<>();
-		for( String column : htblColNameType.keySet()){
-			TableColumn newColumn = new TableColumn(
-					strTableName ,
-					column ,
-					htblColNameType.get(column) ,
-					column.equals(strClusteringKeyColumn) ,
-					null ,
-					null
-			);
-			allColumns.add(newColumn);
-		}
-		Table table = new Table(allColumns);
-	}
+    }
 
 
-	// following method creates a B+tree index 
-	public void createIndex(String   strTableName,
-							String   strColName,
-							String   strIndexName) throws DBAppException{
-		
-		throw new DBAppException("not implemented yet");
-	}
+    // following method creates one table only
+    // strClusteringKeyColumn is the name of the column that will be the primary
+    // key and the clustering column as well. The data type of that column will
+    // be passed in htblColNameType
+    // htblColNameValue will have the column name as key and the data
+    // type as value
+    public void createTable(String strTableName,
+                            String strClusteringKeyColumn,
+                            Hashtable<String, String> htblColNameType) throws DBAppException, IOException {
+        ArrayList<TableColumn> allColumns = new ArrayList<>();
+        for (String column : htblColNameType.keySet()) {
+            TableColumn newColumn = new TableColumn(
+                    strTableName,
+                    column,
+                    htblColNameType.get(column),
+                    column.equals(strClusteringKeyColumn),
+                    null,
+                    null
+            );
+            allColumns.add(newColumn);
+        }
+        Table table = new Table(allColumns);
+    }
 
 
-	// following method inserts one row only. 
-	// htblColNameValue must include a value for the primary key
-	public void insertIntoTable(String strTableName, 
-								Hashtable<String,Object>  htblColNameValue) throws DBAppException{
-	
-		throw new DBAppException("not implemented yet");
-	}
+    // following method creates a B+tree index
+    public void createIndex(String strTableName,
+                            String strColName,
+                            String strIndexName) throws DBAppException {
+
+        throw new DBAppException("not implemented yet");
+    }
 
 
-	// following method updates one row only
-	// htblColNameValue holds the key and new value 
-	// htblColNameValue will not include clustering key as column name
-	// strClusteringKeyValue is the value to look for to find the row to update.
-	public void updateTable(String strTableName, 
-							String strClusteringKeyValue,
-							Hashtable<String,Object> htblColNameValue   )  throws DBAppException{
+    // following method inserts one row only.
+    // htblColNameValue must include a value for the primary key
+    public void insertIntoTable(String strTableName,
+                                Hashtable<String, Object> htblColNameValue) throws DBAppException {
 
-		throw new DBAppException("not implemented yet");
-	}
+        throw new DBAppException("not implemented yet");
+    }
 
 
-	// following method could be used to delete one or more rows.
-	// htblColNameValue holds the key and value. This will be used in search 
-	// to identify which rows/tuples to delete. 	
-	// htblColNameValue enteries are ANDED together
-	public void deleteFromTable(String strTableName, 
-								Hashtable<String,Object> htblColNameValue) throws DBAppException{
-	
-		throw new DBAppException("not implemented yet");
-	}
+    // following method updates one row only
+    // htblColNameValue holds the key and new value
+    // htblColNameValue will not include clustering key as column name
+    // strClusteringKeyValue is the value to look for to find the row to update.
+    public void updateTable(String strTableName,
+                            String strClusteringKeyValue,
+                            Hashtable<String, Object> htblColNameValue) throws DBAppException {
+        String path = "Data_Entry/Tables";
+        File directory = new File(path, strTableName);
+        //checking if the table exists or not
+        //the following implementation assuems that there is no B+ tree index on the column
+        if (directory.exists() && directory.isDirectory()) {
+            System.out.println("table exists!");
+            //if the table directory has only 1 directory it means that it doesn't
+            //have any records in the table
+            if (directory.list().length == 1) {
+                System.out.println("table is empty.");
+            } else {
+                System.out.println("table is not empty.");
+            }
+        } else {
+            System.out.println("table does not exist.");
+        }
+    }
+		throw new
+
+    DBAppException("not implemented yet");
+}
 
 
-	public Iterator selectFromTable(SQLTerm[] arrSQLTerms, 
-									String[]  strarrOperators) throws DBAppException{
-										
-		return null;
-	}
+// following method could be used to delete one or more rows.
+// htblColNameValue holds the key and value. This will be used in search
+// to identify which rows/tuples to delete.
+// htblColNameValue enteries are ANDED together
+public void deleteFromTable(String strTableName,
+                            Hashtable<String, Object> htblColNameValue) throws DBAppException {
+
+    throw new DBAppException("not implemented yet");
+}
 
 
-	public static void main( String[] args ){
-	try{
-			String strTableName = "Cars";
-			DBApp dbApp = new DBApp( );
-			
-			Hashtable htblColNameType = new Hashtable( );
-			htblColNameType.put("id", "java.lang.Integer");
-			htblColNameType.put("name", "java.lang.String");
-			htblColNameType.put("gpa", "java.lang.double");
-			dbApp.createTable( strTableName, "id", htblColNameType );
+public Iterator selectFromTable(SQLTerm[] arrSQLTerms,
+                                String[] strarrOperators) throws DBAppException {
+
+    return null;
+}
+
+
+public static void main(String[] args) {
+    try {
+        String strTableName = "Cars";
+        DBApp dbApp = new DBApp();
+
+        Hashtable htblColNameType = new Hashtable();
+        htblColNameType.put("id", "java.lang.Integer");
+        htblColNameType.put("name", "java.lang.String");
+        htblColNameType.put("gpa", "java.lang.double");
+        dbApp.createTable(strTableName, "id", htblColNameType);
 //			dbApp.createIndex( strTableName, "gpa", "gpaIndex" );
-
 
 
 //			Hashtable htblColNameValue = new Hashtable( );
@@ -162,10 +178,9 @@ public class DBApp {
 //			strarrOperators[0] = "OR";
 //			// select * from Student where name = "John Noor" or gpa = 1.5;
 //			Iterator resultSet = dbApp.selectFromTable(arrSQLTerms , strarrOperators);
-		}
-		catch(Exception exp){
-			exp.printStackTrace( );
-		}
-	}
+    } catch (Exception exp) {
+        exp.printStackTrace();
+    }
+}
 
 }
