7a588d30b8637117efa4240c40833efa45c5826d
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-03-02 11:59:38 +0200

    Table function

--- Data/Table/Table.java
+++ Data/Table/Table.java
@@ -8,6 +8,8 @@ import java.io.*;
 import java.util.ArrayList;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Vector;
 
 
@@ -15,6 +17,7 @@ public class Table implements Serializable {
 
     private static final long serialVersionUID = -9043778273416338053L;
     private Vector<Page> pages ; // page paths
+    private Vector<String> pagePaths ; // page paths
     private transient ArrayList<TableColumn> allColumns;
     static String tablesDirectory = "Data_Entry/Tables";
     private String tableFilePath ;
@@ -89,8 +92,34 @@ public class Table implements Serializable {
         }
         throw new DBAppException("Table not found");
     }
-
-
+//    public Hashtable<Integer, Object> getColIdxVal(Hashtable<String, Object> ht) throws DBAppException {
+//        Hashtable<Integer, Object> res = new Hashtable<>();
+//        for(String key: ht.keySet()){
+//            boolean found = false;
+//            for(int i =0 ; i<getAllColumns().size(); i++){
+//                if(getAllColumns().get(i).equals(key)){
+//                    res.put(i, )
+//                    found = true;
+//                    break;
+//                }
+//            }
+//            if(!found)
+//                throw new DBAppException("Invalid Column Name: " + key);
+//            values.add(ht.get(key));
+//        }
+//    }
+    @Override
+    public boolean equals(Object o){
+        // two tables are equal if they have the same name
+        // to be revisited
+        if(o instanceof Table){
+            return this.tableName.equals(((Table) o).tableName);
+        }
+        if(o instanceof String){
+            return tableName.equals(o);
+        }
+        return false;
+    }
     public static void main(String[] args) throws IOException {
         ArrayList<TableColumn> cols = new ArrayList<>();
         TableColumn col = new TableColumn("test", "cool", "java.lang.String", true, null, null);
