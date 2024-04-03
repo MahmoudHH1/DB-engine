62b085385f5804ac7c6a8363a29e6f085ef79e89
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-04-03 02:44:58 +0200

    dbapp delete and page

--- src/main/java/DBApp.java
+++ src/main/java/DBApp.java
@@ -146,52 +146,56 @@ public class DBApp {
 
         // map column name to idx
         Hashtable<Integer, Object> colIdxVal = table.getColIdxVal(htblColNameValue);
-        for(int i : colIdxVal.keySet()) {
-            if (table.hasIndex(i)) {
-//                allBPlusIndecies.
-                // will hold pointers to matching records
-                Vector<Pointer> idxRemove = null;
-                // hold index of columns with b plus tree
-                ArrayList<Integer> colIdxOfBplus = table.colIdxWBPlus();
-                for (int i : colIdxOfBplus) {
-                    TableColumn col = table.getAllColumns().get(i);
-                    BPlusIndex bplus = IndexControler.readIndexByName(col.getIndexName(), table);
-                    Vector<Pointer> pointers = bplus.search(colIdxVal.get(i));
-                    if (idxRemove == null)
-                        idxRemove = pointers;
-                    else
-                        Operations.intersect(idxRemove, pointers);
-                }
-                colIdxOfBplus.forEach(colIdxVal.keySet()::remove);
+        // will hold pointers to matching records
+        Vector<Pointer> idxRemove = null;
+        // hold index of columns with b plus tree
+        ArrayList<Integer> colIdxOfBplus = table.colIdxWBPlus();
+        for (int i : colIdxOfBplus) {
+            TableColumn col = table.getAllColumns().get(i);
+            BPlusIndex bplus = IndexControler.readIndexByName(col.getIndexName(), table);
+            Vector<Pointer> pointers = bplus.search(colIdxVal.get(i));
+            if (idxRemove == null)
+                idxRemove = pointers;
+            else
+                Operations.intersect(idxRemove, pointers);
+        }
+        colIdxOfBplus.forEach(colIdxVal.keySet()::remove);
 //        colIdxVal.keySet().removeAll(colIdxOfBplus);
-                // if found index
-                if (idxRemove != null) {
-                    idxRemove.sort(Pointer::compareTo);
-                    Page page = null;
-                    for (int i = 0; i < idxRemove.size(); i++) {
-                        if (page == null || !idxRemove.get(i - 1).clusterKeyValue.equals(idxRemove.get(i).clusterKeyValue))
-                            page = (Page) FileCreator.readObject(table.getPagePaths().get(i));
-                        Record record = page.searchRecord(idxRemove.get(i).clusterKeyValue, (int) table.getClusterKeyAndIndex()[1]);
-                        if (record.isMatching(colIdxVal))
-
-
-                    }
-
+        // if found index
+        /* to be optimized further*/
+        if (idxRemove != null) {
+            idxRemove.sort(Pointer::compareTo);
+            Page page = null;
+            ArrayList<Record> toRemove = new ArrayList<>();
+            for (int i = 0; i < idxRemove.size(); i++) {
+                // if no page loaded or need new page then load new page
+                if (page == null || !idxRemove.get(i - 1).clusterKeyValue.equals(idxRemove.get(i).clusterKeyValue)){
+                    if(page != null)
+                        // remove first records
+                        page.removeAll(toRemove);
+                    page = (Page) FileCreator.readObject(table.getPagePaths().get(i));
                 }
+                Record record = page.searchRecord(idxRemove.get(i).clusterKeyValue, (int) table.getClusterKeyAndIndex()[1]);
+                if(record.isMatching(colIdxVal))
+                    toRemove.add(record);
+                page.save();
+            }
+            table.save();
+        }
 
-                for (String path : table.getPagePaths()) {
-                    // still need to adjust for index
-                    Page page = (Page) FileCreator.readObject(path);
-                    ArrayList<Record> toRemove = new ArrayList<>();
-                    for (Record record : page) {
-                        boolean matching = record.isMatching(colIdxVal);
-                        if (matching)
-                            toRemove.add(record);
-                    }
-                    page.removeAll(toRemove);
-                    page.save();
-                }
-                table.save();
+        for (String path : table.getPagePaths()) {
+            // still need to adjust for index
+            Page page = (Page) FileCreator.readObject(path);
+            ArrayList<Record> toRemove = new ArrayList<>();
+            for (Record record : page) {
+                boolean matching = record.isMatching(colIdxVal);
+                if (matching)
+                    toRemove.add(record);
+            }
+            page.removeAll(toRemove);
+            page.save();
+        }
+        table.save();
 
                 //////////////////////////////////////////////////
                 // not completed yet
@@ -206,11 +210,7 @@ public class DBApp {
 //                }
 //            }
 //        }
-            }
-        }
     }
-}
-
 
     public Iterator selectFromTable(SQLTerm[] arrSQLTerms,
                                     String[] strarrOperators) throws DBAppException, IOException, ClassNotFoundException {
